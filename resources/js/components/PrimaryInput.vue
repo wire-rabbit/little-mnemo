2362be71a3ae326314ffe4f5052ead90a64f5c56
Jeremy Bensman <jeremy@wirerabbit.com>
2020-02-01 20:02:11 -0800

    Improve input styles and add alert

--- resources/js/components/PrimaryInput.vue
+++ resources/js/components/PrimaryInput.vue
@@ -1,21 +1,54 @@
 <template>
-    <div class="container">
-        <textarea v-model="numberData"
-                  class="primary-input"
-                  ref="primary_input"
-                  @keydown="nextDigit"
-        ></textarea>
+    <div class="primary-input container-fluid">
+        <div class="row justify-content-center">
+            <div>
+                <textarea v-model="numberData"
+                          rows="1"
+                          class="primary-input"
+                          ref="primary_input"
+                          @keydown="nextDigit"
+                ></textarea>
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <button type="button"
+                    class="btn btn-primary centered fetch-button"
+                    @click="fetchWords"
+            >Fetch Word List</button>
+        </div>
+        <div class="row justify-content-center" v-if="hasErrors">
+            <div class="alert alert-danger" role="alert">
+                <span class="alert-text">Some Kind of Alert</span>
+                <button type="button" class="close" aria-label="Close">
+                    <span aria-hidden="true" @click="dismissAlert">&times;</span>
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 
 <style>
+    .primary-input {
+        font-family: 'Raleway', sans-serif;
+    }
+
+    .alert-text {
+        padding-right: 3em;
+     }
+
+    .fetch-button {
+        padding: 0.5em;
+        margin: 1.5em;
+    }
+
     textarea.primary-input {
-        border: none;
+        border: 2px solid rgba(200, 200, 200, 0.5);
         resize: none;
         outline: none;
         background: transparent;
         font-family: 'Inconsolata', monspace;
         font-size: 4em;
+        text-align: center;
     }
 </style>
 
@@ -29,7 +62,8 @@
         },
         data: function () {
             return {
-                numberData: ''
+                numberData: '',
+                hasErrors: false
             };
         },
         methods: {
@@ -48,6 +82,15 @@
                 } else if (this.numberData.length >= this.maxLength && !acceptedKey) {
                     event.preventDefault();
                 }
+                this.hasErrors = false;
+            },
+            fetchWords: function () {
+                if (this.numberData.length === 0) {
+                    this.hasErrors = true;
+                }
+            },
+            dismissAlert: function () {
+                this.hasErrors = false;
             }
         },
         mounted: function () {
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,6 +5,7 @@
     <title>Little Mnemo - @yield('title')</title>
     <link rel="stylesheet" href="/css/app.css">
     <link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
 </head>
 <body>
     <div id="app">
