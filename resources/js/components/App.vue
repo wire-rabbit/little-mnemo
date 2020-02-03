a4111da16d8bdc55bba28a07743263c1476aa864
Jeremy Bensman <jeremy@wirerabbit.com>
2020-02-02 22:04:09 -0800

    Add word list Vue component

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -6,17 +6,24 @@
                 @fetchWords="fetchWords"
             ></primary-input>
         </div>
+    <word-list :words="matchedWords"></word-list>
     </div>
 </template>
 
 <script>
     export default {
+        data: function () {
+            return {
+                matchedWords: []
+            }
+        },
         methods: {
             fetchWords: function(num) {
+                const that = this;
                 axios.post('/fetch', {
                     number: num
                 }).then(function (response) {
-                    console.log(response.data);
+                    that.matchedWords = response.data;
                 }).catch(function (err) {
                     console.log(err);
                 });
