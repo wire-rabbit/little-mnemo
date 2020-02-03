46acb5dedc92d441803363785cf8f7dccbebb38e
Jeremy Bensman <jeremy@wirerabbit.com>
2020-02-02 21:30:47 -0800

    Complete major system controller

--- app/Http/Controllers/MajorController.php
+++ app/Http/Controllers/MajorController.php
@@ -7,8 +7,16 @@ use App\MajorSystem\MajorSystem;
 
 class MajorController extends Controller
 {
-    public function __invoke(MajorSystem $maj)
+    /**
+     * Simply looks up matches for the POSTed number and returns them as JSON.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \App\MajorSystem\MajorSystem $major
+     * @return string
+     */
+    public function __invoke(Request $request, MajorSystem $major)
     {
-        return json_encode($maj->getMatches(23));
+        $number = $request->input('number');
+        return json_encode($major->getMatches($number));
     }
 }
